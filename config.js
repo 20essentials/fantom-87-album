bf7b470881786439533f0a7ad90948e77980dd62
DominicDev <s0l0estudi4r@gmail.com>
2025-09-10 17:54:11 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' FANTOM ‘87 ';
+export let nameSongs = [
+	"PAY PHONE", /*1*/
+	"ARRIVAL", /*2*/
+	"DESTINATION", /*3*/
+	"DISCOVERY", /*4*/
+	"GEMINI", /*5*/
+	"OH, DREAMER", /*6*/
+	"HOME", /*7*/
+	"SOLAR", /*8*/
+	"ECLIPSE RIDER", /*9*/
+	"LOVE TRANSMISSION", /*10*/
+	"ORBITER", /*11*/
+	"QUICK LOVE", /*12*/
+	"LAZER BOMB", /*13*/
+	"SELECT START", /*14*/
+	"TERRAFORM", /*15*/
+	"MASK", /*16*/
+	"TRAVELLER", /*17*/
+	"DEPERSONALIZE", /*18*/
+	"AMETHYST PLANET", /*19*/
+	"PROTONIC REVERSAL", /*20*/
+]
